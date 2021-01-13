421538618f796ebc8ae267ec4f1a0925e0699ab6
kovey <kovey@vip.qq.com>
2021-01-13 13:56:30 +0800

    bugs fixed

--- src/App/Bootstrap/Bootstrap.php
+++ src/App/Bootstrap/Bootstrap.php
@@ -49,10 +49,10 @@ class Bootstrap
     public function __initLogger(Application $app)
     {
         ko_change_process_name(Manager::get('server.server.name') . ' root');
-        Logger::setLogPath(Manager::get('server.logger_dir'));
+        Logger::setLogPath(Manager::get('server.server.logger_dir'));
         Logger::setCategory(Manager::get('server.server.name'));
-        Monitor::setLogDir(Manager::get('server.logger_dir'));
-        Db::setLogDir(Manager::get('server.logger_dir'));
+        Monitor::setLogDir(Manager::get('server.error.ogger_dir'));
+        Db::setLogDir(Manager::get('server.server.logger_dir'));
 
         if (Manager::get('server.session.open') === 'On' && Manager::get('server.session.type') === 'file') {
             if (!is_dir(Manager::get('server.session.dir'))) {
